bc21fa66f979a1713b839e17c92946c314a939c0
Arvin Buid <arvinbuid01@gmail.com>
2025-06-05 00:15:05 +0800

    Add the html, css, and the text content of  Home component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,33 @@
+'use client'
+
+import styles from './page.module.css'
+
+const phrases = [
+  "It is a long established fact",
+  "that a reader will be distracted",
+  "by the readable content of a page",
+  "when looking at its layout."
+]
+
 export default function Home() {
   return (
-    <>Home</>
+    <div className={styles.container}>
+      <MaskText />
+      <MaskText />
+      <MaskText />
+      <MaskText />
+    </div>
   );
+}
+
+export function MaskText() {
+  return (
+    <div className={styles.body}>
+      {phrases.map((phrase, index) => (
+        <div key={index} className={styles.lineMask}>
+          <p>{phrase}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
